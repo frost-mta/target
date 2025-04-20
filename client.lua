4140117181695d87503d8352661d1091b28d1d86
frost-mta <frostdev.mta@gmail.com>
2025-04-20 15:54:57 -0300

    Add files via upload
    Sistema de target v0.2 
    
    [Novidades]
    - Colocado no config para poder mudar a cor do target 
    - Colocado no config para poder aumentar o tamanho do target
    - Colocado no config para poder mudar a tecla de pressionar para ativar o target

--- client.lua
+++ client.lua
@@ -1,18 +1,17 @@
 
 local screenWidth, screenHeight = guiGetScreenSize()
-local imageWidth, imageHeight = 15, 15 
 local isVisibleTarget = false 
 
 
 function drawTargetImage()
     if isVisibleTarget then
-        dxDrawImage(1011, 475, imageWidth, imageHeight, "main/image/target.png", 0, 0, 0, targetColor)
+        dxDrawImage(1011, 475, config.targetSize.width, config.targetSize.height, "main/image/target.png", 0, 0, 0, config.targetColor)
     end
 end
 
 function toggleTargetImage(key, press)
-    if key == "lalt" then 
-        isVisibleTarget = press 
+    if key == config.keyToggle then 
+        isVisibleTarget = press
     end
 end
 addEventHandler("onClientRender", root, drawTargetImage)
--- shared.lua
+++ shared.lua
@@ -1,1 +1,9 @@
-targetColor = tocolor(255, 255, 255, 255) 
+config = {
+    targetColor = tocolor(255, 255, 255, 255),
+    
+    targetSize = {
+        width = 20, -- Largura do target
+        height = 20, -- Altura do target
+    },
+    keyToggle = "lalt",
+}
